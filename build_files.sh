571c660f873336ce1baa9c4adf1d1d4378c5c841
todhritesh <todhritesh@gmail.com>
2023-02-21 18:53:12 +0530

    added postgresql 1

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 pip install -r requirements.txt
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate  --noinput
-python3.9 manage.py collectstatic --noinput --clean
+python3.9 manage.py collectstatic --noinput
