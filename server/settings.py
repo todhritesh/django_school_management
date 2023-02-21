b468ec03cbe70dfc0fb6e5b7a31da69c07ef2d9c
todhritesh <todhritesh@gmail.com>
2023-02-21 15:31:44 +0530

    added vercel config

--- server/settings.py
+++ server/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-f2jb2p+pwj5!*_fitjtieb#iv&dh-cx@s5oo+cw$=#x#^*#(fz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 # Application definition
 
@@ -130,6 +129,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     BASE_DIR / "static/"
 ]
+
+STATIC_ROOT = BASE_DIR/'staticfiles'
 
 # CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
